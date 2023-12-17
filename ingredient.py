5216adbc3afc57914e2acc9720afaf536e1f0cbe
PhilippFeO <email@example.com>
2023-12-17 19:55:47 +0100

    Feat: Generate shopping list, sort via category_weight

--- ingredient.py
+++ ingredient.py
@@ -1,18 +1,69 @@
 import yaml
+import os
 
 
 class Ingredient:
-    field_names = ["Name", "Menge", "Kategorie", "Optional"]
+    field_names = ["?", "Name", "Menge", "Kategorie", "Gericht"]
+    _padding = 15
+    _space_column_width = 3
+    _category_weights = {'vegetable': 10,
+                         'fruit': 9,
+                         'diary': 8,
+                         'noodles': 6,
+                         'seasoning': 3,
+                         'alltagsartikel': 2,
+                         'tiefkühlprodukt': 1}
 
-    def __init__(self, name, amount, category, optional=False):
+    def __init__(self, name, amount, category, optional=False, meal=''):
         self.name = name
         self.amount = str(amount)
         self.category = category
         self.optional = optional
+        self.meal = meal
+        # Assign category weight accoirding to order in supermarket (=> walk from
+        # category to category to be efficient).
+        # This key is is used for sorting the ingredients when generating the shopping list.
+        try:
+            self.category_weight = self._category_weights[category.lower()]
+        # Maybe a category is missing, then weight defaults to 0 and has to be added manually.
+        except KeyError:
+            self.category_weight = 0
+            # Print information about missung category
+            s = f"Key\n\t{category.lower()}\nnot found in\n"
+            cw_dict = '\n\t'.join(f"{k}: {v}"
+                                  for k, v in self._category_weights.items())
+            print('\t'.join((s, cw_dict)))
 
     def __str__(self):
-        optional_str = " (Optional)" if self.optional else ""
-        return f"{self.name}: {self.amount} ({self.category}){optional_str}"
+        return Ingredient.to_table_string([self.optional,
+                                           self.name,
+                                           self.amount,
+                                           self.category,
+                                           self.meal])
+
+    # Also used for crafting a header for the table, hence class method and first argument as list
+    # (The field names are provided as list, attributes follow)
+    # Padding, to have a small neat table
+    @ classmethod
+    def to_table_string(cls, attributes: list = field_names):
+        """
+        Format elements (field names, ingredient) as string for shopping list.
+        """
+        optional = attributes[0]
+        name = attributes[1]
+        amount = attributes[2]
+        category = attributes[3]
+        meal = attributes[4]
+        # Cap at _padding, no matter what (keep in mind when comosing a recipe)
+        pad = Ingredient._padding
+        scw = Ingredient._space_column_width
+        s = '?' if optional == '?' else '1' if optional else ' '
+        s = (' ' * scw).join((s, *(f"{attr[:pad]:<{pad}}"
+                                   for attr in [name,
+                                                amount,
+                                                category])))
+        s = ' '.join((s, meal))
+        return s
 
 
 def read_ingredients(file_path):
@@ -21,11 +72,13 @@ def read_ingredients(file_path):
         recipe_data = yaml.safe_load(file)
 
     ingredients = recipe_data.get("ingredients", [])
-    return [Ingredient(**ingredient) for ingredient in ingredients]
+    # basename provides file name, splittext separates name and extension, [0] uses plain file name
+    return [Ingredient(**ingredient, meal=os.path.splitext(os.path.basename(file_path))[0])
+            for ingredient in ingredients]
 
 
 if __name__ == "__main__":
-    file_path = "recipes/bowl.yaml"
+    file_path = "recipes/spaghetti.yaml"
     ingredients = read_ingredients(file_path)
 
     for i in ingredients:
--- retrieve_ingredients.py
+++ retrieve_ingredients.py
@@ -26,31 +26,14 @@ def main():
         # Append the ingredients to the superlist
         all_ingredients.extend(ingredients)
         all_ingredients = sorted(all_ingredients,
-                                 key=lambda ingredient: ingredient.category)
+                                 key=lambda ingredient: ingredient.category_weight,
+                                 reverse=True)
 
     # Save list of ingredients
-    # find longest element for proper formatting of the output
-    max_len_name = max((len(i.name) for i in all_ingredients)) + 2
-    # Print a header/column names
-    header = ' '.join((fn.ljust(max_len_name)
-                      for fn in Ingredient.field_names))
-    print(header)
-    print('-' * len(header))
-    # Print rows
-    for ingredient in all_ingredients:
-        # apply padding according to longest entry in table
-        print(ingredient.name.ljust(max_len_name),
-              ingredient.amount.ljust(max_len_name),
-              ingredient.category.ljust(max_len_name),
-              ingredient.optional)
-
-    # TODO: Write table into text file <15-12-2023>
-    # TODO: Add name of recipe to each ingredient <15-12-2023>
-    #   Maybe by adding a recipe attribute to the Ingredient class
-    # TODO: Add weight to each ingredient to sort according to order in supermarket <15-12-2023>
-    #   fi. vegetables and fruits come first, then diary, etc.
-    # TODO: Add method similar to __str__ to Ingredient to construct the string for printing. This method might take one argument, the padding length, in my case max_len_name <15-12-2023>
-        # TODO: return this string in __str__ <15-12-2023>
+    header = Ingredient.to_table_string()
+    with open('shopping_list.txt', 'w') as sl:
+        sl.write(f"{header}\n\n")
+        sl.writelines((f"{ingredient}\n" for ingredient in all_ingredients))
 
 
 if __name__ == "__main__":
