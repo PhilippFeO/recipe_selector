ab191eb37f94d37419b170c14351839b8a438d3c
PhilippFeO <email@example.com>
2024-01-02 16:48:10 +0100

    wip: Obtain category, url from a csv file

--- ingredient.py
+++ ingredient.py
@@ -8,30 +8,47 @@ class Ingredient:
     _name_col_num = field_names.index('Name') + 1
     _padding = 15
     _space_column_width = 3
-    _category_weights = None
+    _category_weights: dict[str, int] = None
+    _category_weights_file: str = 'res/category_weights.csv'
+    _icu: dict[str, str, str] = None  # i=ingredient, c=category, u=url
+    _icu_file: str = 'res/ingredient_category_url.csv'
 
     def __init__(self, name, quantity, category, url='HIER KÖNNTE IHRE URL STEHEN', optional=False, meal=''):
         self.name = name
         self.quantity = str(quantity)
-        self.category = category
-        self.url = url
+        self.category = ''
+        self.url = ''
         self.optional = optional
         self.meal = meal
+        # Fill self.{category, url}
+        if Ingredient._icu is None:
+            Ingredient._icu = read_csv(Ingredient._icu_file, to_int=False)
+        try:
+            self.category = Ingredient._icu[self.name][0]
+            self.url = Ingredient._icu[self.name][1]
+        except KeyError:
+            print(f'No {self.category} in {Ingredient._icu_file}')
+            print(f'\tField {self.name}.category left empty')
+            print(f'\tField {self.name}.url left empty')
         # Assign category weight accoirding to order in supermarket (=> walk from
         # category to category to be efficient).
         # This key is is used for sorting the ingredients when generating the shopping list.
         # Maybe a category i{}s missing, then weight defaults to 0 and has to be added manually.
-        Ingredient._category_weights = read_csv('res/category_weights.csv', to_int=True)
-        try:
-            self.category_weight = Ingredient._category_weights[category.lower()]
-        except KeyError:
-            self.category_weight = 0
-            # Print information about missung category
-            # TODO: accomplish this via logging <23-12-2023>
-            s = f"Key\n\t{self.category.lower()}\t({self.name})\nnot found in\n"
-            cw_dict = '\n\t'.join(f"{k}: {v}"
-                                  for k, v in self._category_weights.items())
-            print('\t'.join((s, cw_dict)), file=sys.stderr)
+        if Ingredient._category_weights is None:
+            Ingredient._category_weights = read_csv(Ingredient._category_weights_file, to_int=True)
+        # Without self.category set, quering for the weight doesn't make sense
+        if self.category and self.url:
+            try:
+                self.category_weight = Ingredient._category_weights[self.category]
+            except KeyError:
+                self.category_weight = 0
+                # Print information about missung category
+                # TODO: accomplish this via logging <23-12-2023>
+                # s = f"Key\n\t{self.category.lower()}\t({self.name})\nnot found in\n"
+                # cw_dict = '\n\t'.join(f"{k}: {v}"
+                #                       for k, v in self._category_weights.items())
+                # print('\t'.join((s, cw_dict)), file=sys.stderr)
+                print(f'{self.category} not found in {Ingredient._category_weights_file}')
 
     def __str__(self):
         return Ingredient.to_table_string([self.optional,
--- read_csv.py
+++ read_csv.py
@@ -13,10 +13,14 @@ def read_csv(file_path: str, to_int: bool) -> dict[str, str]:
         csv_reader = csv.reader(csv_file)
 
         for row in csv_reader:
-            # Other values indicate comments, etc.
+            # Other values indicate comments, etc. or another file (s. below)
             if len(row) == 2:
                 key, value = row
                 csv_dict[key] = int(value) if to_int else value
+            # Used when reading 'res/ingredient_category_url.csv'
+            if len(row) == 3:
+                ingredient, category, url = row
+                csv_dict[ingredient] = (category, url)
 
     return csv_dict
 
