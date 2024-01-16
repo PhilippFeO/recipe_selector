1f100ac7ede21aa2e692f6b7717ff8deabb46239
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-01-16 21:38:18 +0100

    Update: Open URLs with a firefox profile
    Before all URLs were opened with the default firefox profile (because no
    '--profile' option was used). Now, it does so with a specific
    supermarket profile. Hence, it doesn't interfere with the normal
    browsing and one can keep passwords and cookies.

--- open_urls.py
+++ open_urls.py
@@ -1,19 +1,24 @@
-import webbrowser
+import subprocess
 
 
 def open_ingredient_urls(ingredient_names: list[str], icu_dict: dict[str, str]):
-    firefox_path = "/usr/bin/firefox"
-    webbrowser.register('firefox', None, webbrowser.BackgroundBrowser(firefox_path))
+    """
+    Open ingredients, ie. their urls in firefox with the supermarket profile (in my case with "Rewe")
+    """
+    # TODO: Profilpfad in Config-Datei auslagern <16-01-2024>
+    firefox = "firefox --profile /home/philipp/.mozilla/firefox/5mud7ety.Rewe"
 
-    # Open browser, ie. landing page to log in
-    landing_page = 'www.wikipedia.org'
-    webbrowser.get('firefox').open_new(landing_page)
+    # TODO: Prüfen, ob diese notwendig ist <16-01-2024>
+    #   Ich glaube, man kann Produkte „anonym“ in Warenkorb leben, weil man beim Bezahlen automatisch zur Anmeldung geleitet wird.
+    #   Aktueller Stand: Erst anmelden und dann jede Seite aktualisieren bevor man Produkt in Warenkorb legt
+    landing_page = 'https://account.rewe.de/realms/sso/protocol/openid-connect/auth?response_type=code&client_id=ecom&redirect_uri=https%3A%2F%2Fwww.rewe.de%2Fsso%2Flogin&state=4f44fbdb-c2bd-45d1-a142-771f02433d39&login=true&scope=openid'
 
-    # Open each URL in Firefox
+    # Open each URL
+    urls = [landing_page]
     for ing_name in ingredient_names:
         try:
-            url = icu_dict[ing_name][1]
+            urls.append(icu_dict[ing_name][1])
         except KeyError:
             print(f'Ingredient "{ing_name}" has no url.')
-            continue  # No ingredient_name in dict => continue loop
-        webbrowser.get('firefox').open_new_tab(url)
+    # subpress warnings from firefox
+    subprocess.run([*firefox.split(' '), *urls], stderr=subprocess.DEVNULL)
