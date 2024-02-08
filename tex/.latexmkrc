00f01a6cc957b0de1415a8cb47615332c26bbdd0
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-02-08 23:04:58 +0100

    ✨ Feat(latex): Compile multiple recipes at once

--- tex/.latexmkrc
+++
@@ -1,3 +0,0 @@
-$out_dir='out';
-# Link pdf from out/ into ./
-$compiling_cmd='ln --force %Z/%R.pdf ./%R.pdf';
--- tex/compile_recipe.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Compile CLI provided tex files of recipes.
-
-latexmk -verbose -file-line-error -interaction=nonstopmode "$@"
