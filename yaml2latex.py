6f19cd6fb029f7516c4fd8c8694ba690452ca903
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-02-09 22:22:36 +0100

    🐛 Fix(latex): Use correct German quotation marks
    Before, „ & “  were skipped and warning was issued. Now, „ & “ are
    replaced (s. code) and displayed correctly.

--- yaml2latex.py
+++ yaml2latex.py
@@ -11,7 +11,7 @@ class Recipe:
         self.recipe_name = recipe_name
         self.ingredients: list[Ingredient] = [Ingredient(**ingredient)
                                               for ingredient in ingredients]
-        self.preparation: list[str] = preparation
+        self.preparation: list[str] = [step.replace('„', '"`').replace('“', '"\'') for step in preparation]
 
     def to_latex(self):
         # TODO: Write strings to pipe, read form pipe in latex file <06-02-2024>
