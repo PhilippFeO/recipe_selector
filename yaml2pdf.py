03e15d47710324ce73a86f441f7889ef40ea056f
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-02-13 21:11:59 +0100

    🎨 Improve(latex): Better spacing for odd ingredients
    Before, I used 'itemize' and 'multicols' to have two columns of
    ingredients. An odd number of ingredients (in the second column) was rendered with different spacing between the items than the first column has.
    
    Now, this is fixed by using 'tabularx' and leaving the last field empty.

--- yaml2pdf.py
+++ yaml2pdf.py
@@ -4,6 +4,7 @@ import subprocess
 import logging
 from pathlib import Path
 from ingredient import Ingredient
+from itertools import zip_longest
 
 
 class Recipe:
@@ -19,18 +20,45 @@ class Recipe:
     def to_latex(self):
         # TODO: Write strings to pipe, read form pipe in latex file <06-02-2024>
         res_dir = 'res'
+        # Recipe title
         with open(f'{res_dir}/title.tex', 'w') as recipe_name_file:
             recipe_name_file.write(self.recipe_name)
-        with open(f'{res_dir}/ingredients.tex', 'w') as ingredients_file:
-            # sorted sets False before True
-            ings_sorted = sorted(self.ingredients, key=lambda ing: ing.optional)
-            ingredients_file.writelines(
-                (f'\\item {ing.quantity} {ing.name}\n'
-                    if not ing.optional
-                 else f'\\item \\textcolor{{gray}}{{{ing.quantity} {ing.name}}}\n'
-                 for ing in ings_sorted))
+        # Preparation
         with open(f'{res_dir}/preparation.tex', 'w') as preparation_file:
+            # Each step starts with '\d{1,2}.\s', ie 3-4 chars
+            # TODO: Adjust slice according to above comment <13-02-2024, Philipp Rost>
+            #   Idea: 'preparation' object containing number and instruction separately
+            # Works because Latex collapses multiple spaces (after `\item`) into one
             preparation_file.writelines((f'\\item {step[3:]}\n' for step in self.preparation))
+
+        # Ingredients
+        table_body = ''
+
+        # Make optional ingredients gray
+        def color_ingredient(ing: Ingredient) -> str:
+            """ Dye optional ingredients gray. """
+            if ing and ing.optional:
+                s = f'\\textcolor{{gray}}{{- {ing.quantity} {ing.name}}}'
+            elif ing:
+                s = f'- {ing.quantity} {ing.name}'
+            else:
+                s = ''
+            return s
+
+        # `sorted()` sets False before True
+        ings_sorted = sorted(self.ingredients, key=lambda ing: ing.optional)
+        # I prefere having optional (gray) ingredients in the right column, ie. I have to split
+        # `ings_sorted` in half and align both sublists as columns or do some index mangling.
+        # In case of odd number of ingredients, `fillvalue` enhances the second shorter iterable
+        middle_idx = len(ings_sorted) // 2
+        for ing1, ing2 in zip_longest(ings_sorted[:middle_idx],
+                                      ings_sorted[middle_idx:],
+                                      fillvalue=None):
+            ing1_as_field, ing2_as_field = color_ingredient(ing1), color_ingredient(ing2)
+            table_row = f'{ing1_as_field} & {ing2_as_field}\\\\\n'
+            table_body += table_row
+        with open(f'{res_dir}/ingredients.tex', 'w') as ingredients_file:
+            ingredients_file.writelines(table_body)
 
 
 def read_recipe(file_path):
@@ -40,7 +68,6 @@ def read_recipe(file_path):
 
 
 if __name__ == '__main__':
-    p = argparse.ArgumentParser("isolate_shape")
     p = argparse.ArgumentParser(__name__)
     p.add_argument("recipe_yaml",
                    help="One or more yaml files containing a recipe.",
